54e346eff59fb30a842b13369019167ecabf809f
Yao Chi <yaochi@hnrfqj.com>
2020-11-02 18:45:23 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,35 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
+
+let instance = null
+function render(props) {
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app'); // 这里是挂载到自己的html中 基座会拿到这个挂载后的html 将其插入进去
+}
+
+if (window.__POWERED_BY_QIANKUN__) { // 动态添加publicPath
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
+
+if (!window.__POWERED_BY_QIANKUN__) { // 默认独立运行
+  render();
+}
+
+export async function bootstrap(props) {
+  console.log(props)
+}
+
+export async function mount(props) {
+  render(props)
+}
+
+export async function unmount(props) {
+  instance.$destroy()
+}
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    port: 10002,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: 'vueApp',
+      libraryTarget: 'umd'
+    }
+  }
+}
